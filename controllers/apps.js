626a06306c369f79cb7b435c0888107bbff3fba3
ilyosbek9531 <iqboljonsheraliyev11@gmail.com>
2023-09-15 11:03:17 +0500

    fixed

--- controllers/apps.js
+++ controllers/apps.js
@@ -50,36 +50,6 @@ async function putApp(req, res) {
   }
 }
 
-async function patchApp(req, res) {
-  try {
-    const appId = req.params.id;
-    const { name, tab_name, icon } = req.body;
-
-    const app = await User.findById(appId);
-
-    if (!app) {
-      return res.status(404).json({ message: "App not found" });
-    }
-
-    if (name) {
-      user.name = name;
-    }
-
-    if (tab_name) {
-      user.tab_name = tab_name;
-    }
-
-    if (icon) {
-      user.icon = icon;
-    }
-
-    await app.save();
-    res.status(200).json(app);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-}
-
 async function deleteApp(req, res) {
   try {
     const appId = req.params.id;
@@ -102,6 +72,5 @@ module.exports = {
   postApp,
   getSingleApp,
   putApp,
-  patchApp,
   deleteApp,
 };
--- routes/apps.js
+++ routes/apps.js
@@ -6,7 +6,6 @@ router.get("/", appsController.getApps);
 router.post("/", appsController.postApp);
 router.get("/:id", appsController.getSingleApp);
 router.put("/:id", appsController.putApp);
-router.patch("/:id", appsController.patchApp);
 router.delete("/:id", appsController.deleteApp);
 
 module.exports = router;
